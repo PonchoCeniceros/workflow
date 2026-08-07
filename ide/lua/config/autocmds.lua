3c478fb8fd33dce4e71314e28952472a14ad2ce1
Giovanny Alfonso Chávez Ceniceros <70242956+PonchoCeniceros@users.noreply.github.com>
2026-08-07 13:41:14 -0600

    [main] desactivar spell-check en markdown
    LazyVim activa el corrector ortografico por defecto en buffers
    markdown; se agrega un autocmd que lo apaga (vim.opt_local.spell)
    para dejar de ver los subrayados de "errores" ortograficos.

--- ide/lua/config/autocmds.lua
+++ ide/lua/config/autocmds.lua
@@ -2,6 +2,14 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- desactivar el corrector ortografico en markdown (LazyVim lo activa por defecto)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 -- inicializacion del plugin de csv
 vim.api.nvim_create_autocmd("BufRead", {
   pattern = "*.csv",
